37b78e2f8efd49fee8a6a62df1c682a23af7ba8e
Sunil Kumar <suh9708@bswhealth.org>
2020-07-31 15:16:17 +0530

    initial setup of redux and woring copy

---
+++ ios/Pods/Target Support Files/Pods-MyBSW-tvOSTests/Pods-MyBSW-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyBSW_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyBSW_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-MyBSW/Pods-MyBSW-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyBSW : NSObject
+@end
+@implementation PodsDummy_Pods_MyBSW
+@end
